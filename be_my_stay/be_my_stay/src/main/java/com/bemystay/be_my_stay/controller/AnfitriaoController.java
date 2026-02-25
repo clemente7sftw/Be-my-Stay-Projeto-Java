0da193ad15e16cef4618db8c07811fa55e699204
clemente7sftw <gabiclemente2007@gmail.com>
2026-02-25 14:30:51 -0300

    Adicionando Perfil para Anfitriões

--- be_my_stay/be_my_stay/src/main/java/com/bemystay/be_my_stay/controller/AnfitriaoController.java
+++ be_my_stay/be_my_stay/src/main/java/com/bemystay/be_my_stay/controller/AnfitriaoController.java
@@ -11,7 +11,13 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Controller
@@ -72,6 +78,56 @@ public class AnfitriaoController {
         imovelService.ativar(id);
         return "redirect:/anfitriao/listarImoveis";
     }
+
+    @GetMapping("/perfil")
+    public String perfil(HttpSession session, Model model) {
+        Long idUsuario = (Long) session.getAttribute("idUsuario");
+        if (idUsuario == null) {
+            return "redirect:/usuarios/login";
+        }
+
+        Usuario usuario = usuarioService.buscarPorId(idUsuario);
+
+        model.addAttribute("usuario", usuario);
+
+
+        return "anfitriao/perfil";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        Usuario u = usuarioService.buscarPorId(id);
+        model.addAttribute("usuario", u);
+
+        return "anfitriao/perfilEdicao";
+
+    }
+    @PostMapping("/salvarEdicao/{id}")
+    public String salvarEdicao(
+            @PathVariable Long id,
+            @ModelAttribute Usuario usuario,
+            @RequestParam(value = "arquivo", required = false) MultipartFile file
+    ) throws IOException {
+
+        if (file != null && !file.isEmpty()) {
+            String nome = file.getOriginalFilename();
+
+            Path caminho = Paths.get(
+                    "src/main/resources/static/uploads/fotos_perfil/" + nome
+            );
+
+            Files.createDirectories(caminho.getParent());
+            Files.copy(file.getInputStream(), caminho, StandardCopyOption.REPLACE_EXISTING);
+
+            usuario.setFoto_perfil("fotos_perfil/" + nome);
+        }
+
+
+        usuarioService.editar(id, usuario);
+
+        return "redirect:/anfitriao/perfil";
+    }
+
 
 
 
