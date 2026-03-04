718b62069ba0a221d09dcb19f8e66e6d1bcf6eac
clemente7sftw <gabiclemente2007@gmail.com>
2026-03-04 17:43:11 -0300

    Refatorando CSS dev relatório

--- be_my_stay/be_my_stay/src/main/java/com/bemystay/be_my_stay/BeMyStayApplication.java
+++ be_my_stay/be_my_stay/src/main/java/com/bemystay/be_my_stay/BeMyStayApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BeMyStayApplication {
+public class  BeMyStayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeMyStayApplication.class, args);
